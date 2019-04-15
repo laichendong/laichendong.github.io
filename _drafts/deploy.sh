a6f9de72a240d7cb7efa915cd843df9ab7712150
laichendong <laichendong@jd.com>
2019-04-15 20:51:08 +0800

    commit:

--- _drafts/deploy.sh
+++ _drafts/deploy.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 work_dir=`pwd`
+if [[ $1 -eq "-h"  ]]; then
+	echo "useage: ./deploy.sh {draft_file_name}"
+	exit 0
+fi
 
 if [[ ! ${work_dir: -8} == "/_drafts" ]]; then
-	echo "请把脚本放到_drafts目录下执行"
+	echo "请把脚本放到_drafts目录下执行 useage: ./deploy.sh {draft_file_name}"
 	exit 0
 fi
 
 if [[ $# != 1 ]]; then
-	echo "请输入要发布的草稿文件名"
+	echo "请输入要发布的草稿文件名 useage: ./deploy.sh {draft_file_name}"
 else
 	f=$1
 fi
@@ -17,5 +21,6 @@ if [[ ! -f "$f" ]]; then
 else
 	fn=`date +%Y-%m-%d`-$f
 	mv $f "../_posts/$fn"
-	ls -l ../_posts/$fn
+	echo "文件已发布到:" `ls -l ../_posts/$fn`
+	
 fi
